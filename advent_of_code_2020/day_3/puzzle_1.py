37a92b9a1926c2afcf746a18ea31f81f6cfea8cc
Paulo-A <pauloeduardopandrade@gmail.com>
2021-01-30 21:10:52 -0300

    Code cleanup

--- advent_of_code_2020/day_3/puzzle_1.py
+++ advent_of_code_2020/day_3/puzzle_1.py
@@ -52,8 +52,6 @@ In this example, traversing the map using this slope would cause you to encounte
 Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
 """
 
-import csv
-
 SLOPE = [3, 1]
 
 class ForestMap(list):
